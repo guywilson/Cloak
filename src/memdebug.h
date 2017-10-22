44d2d11ecff227b76427c706cb52b96bd9f1850a
Guy Wilson <wilson.guy@gmail.com>
2017-10-22 16:39:09 +0100

    Delete memdebug.h

--- src/memdebug.h
+++
@@ -1,5 +0,0 @@
-void * malloc_d(size_t size, const char *pszId);
-void free_d(void *ptr, const char *pszId);
-void * memcpy_d(void * d, void * s, size_t size, const char *pszId);
-size_t fwrite_d(const void * ptr, size_t size, size_t count, FILE * stream, const char *pszId);
-size_t fread_d(void * ptr, size_t size, size_t count, FILE * stream, const char *pszId);
